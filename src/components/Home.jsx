3f6fdbf08f45ccaf805acdcfdb37cc0878d649b0
xAldox995 <valzanialdo@gmail.com>
2024-08-30 12:27:51 +0200

    degli abozzi

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+
+
+const Home = () => {
+    //uso unalocation di default
+    const userLocation = 'Rome'
+
+    return(
+        <div className="text-center">
+            <h1>My Weather App</h1>
+            <div>
+                {/* <SearchBarComp/>
+                <WeatherCard location = {userLocation}/> */}
+            </div>
+        </div>
+    )
+}
+
+export default Home
